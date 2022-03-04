637572d7c953a49ac9ff35f966380d98e21c9d1e
Jaroslav Šmolík <grissius@gmail.com>
2022-03-04 23:20:41 +0100

    wip

---
+++ src/domain/interactors/list-posts.test.ts
@@ -0,0 +1,8 @@
+import { postMemoryRepository } from "../../infra/repositories/post-repo.memory"
+import { Post } from "../entities/post"
+
+it('List posts', async () => {
+    // const INPUT: ListPostsInput = {}
+    // await Promise.all(Array(100).fill(0).map((_, i) => postMemoryRepository.createPost(Post.NewPost({ userId: '', title: `title-${i}`, text: `text-${i}` }))))
+    // const result = await ListPosts(INPUT)
+})
